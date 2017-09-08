ab252fb2e4e35704fa15843be46ed814c7f7fa6d
timmycheng <timmycheng@foxmail.com>
2017-09-08 17:28:10 +0800

    上传功能完成

--- app/controllers/item.js
+++ app/controllers/item.js
@@ -1,4 +1,12 @@
 var Item = require('../models/item')
+var COS = require('cos-nodejs-sdk-v5')
+var fs = require('fs')
+
+var cos = new COS({
+    AppId: '1251622208',
+    SecretId: 'AKID4JEDFLKH91pW7gh6proZJySkqmpVZkp2',
+    SecretKey: 'mW5BD3iD6xkPa2Y2KXJ0Wq7FBuSnTPDo',
+})
 
 exports.new = function(req, res){
     res.render('new', {
@@ -8,8 +16,61 @@ exports.new = function(req, res){
 
 exports.save = function(req, res){
     var itemObj = req.body.item
-    var itemPics = req.files
-    console.log(itemObj)
-    console.log(itemPics)
-    res.send('ok')
+    var itemPics = req.files.pics
+    var _item = new Item(itemObj)
+    // var _path
+    _item.save(function(err, item){
+        if(err){
+            console.log(err)
+        }
+        // _path = item._id
+        if(typeof(itemPics.length) === 'undefined'){
+            cos.putObject({
+                Bucket: 'xingu',
+                Region: 'ap-shanghai',
+                // Key: 'photos/' + _path + '/' + itemPics.originalFilename,
+                Key: 'photos/' + itemObj.name + '/' + itemPics.originalFilename,
+                ContentLength: itemPics.size,
+                Body: fs.createReadStream(itemPics.path),
+                // onProgress: function(progressData){
+                //     console.log(JSON.stringify(progressData))
+                // }
+            }, function(err, data){
+                console.log(err, data)
+                fs.unlinkSync(itemPics.path)
+            })
+        }else{
+            console.log('multiple upload')
+            for (var i = 0; i < itemPics.length; i++) {
+                var pic = itemPics[i];
+                // console.log(pic.originalFilename)
+                // console.log(pic.path)
+                // console.log(pic.size)
+                cos.putObject({
+                    Bucket: 'xingu',
+                    Region: 'ap-shanghai',
+                    // Key: 'photos/' + _path + '/' + pic.originalFilename,
+                    Key: 'photos/' + itemObj.name + '/' + pic.originalFilename,
+                    ContentLength: pic.size,
+                    Body: fs.createReadStream(pic.path),
+                    // onProgress: function(progressData){
+                    //     console.log(JSON.stringify(progressData))
+                    // }
+                }, function(err, data){
+                    console.log(err, data)
+                    try{
+                        fs.unlinkSync(pic.path)
+                    }catch (err){
+                        console.log(err)
+                    }
+                })
+            }
+        }
+    })
+    // console.log(_path)
+    // console.log(itemObj)
+    // console.log(itemPics.length)
+    // console.log(itemPics[0])
+    res.redirect('/')
+                            
 }
--- public/js/new.js
+++ public/js/new.js
@@ -1,17 +1,17 @@
 $(function(){
     $("#itemPictures").fileinput({
-        uploadUrl:"/photo",
+        // uploadUrl:"/photo",
         showCaption: false,
-        autoReplace: true,
+        // autoReplace: true,
         maxFileCount: 9,
         showUpload: false,
         showRemove: false,
         dropZoneEnabled: false,
-        // enctype: 'multipart/form-data',
+        enctype: 'multipart/form-data',
         allowedFileExtensions: ['jpg', 'png', 'gif']
     })
-    $('#picup').click(function(){
-        console.log('upload pics')
-        $('#itemPictures').fileinput("upload")
-    })
+    // $('#picup').click(function(){
+    //     console.log('upload pics')
+    //     $('#itemPictures').fileinput("upload")
+    // })
 })
